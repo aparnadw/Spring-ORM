e670b10c78746cd8786b33a2073ae09c5980e458
aparnadw <aparnadw03@gmail.com>
2024-06-10 23:07:26 +0530

    inital commit for spring ORM

---
+++ springorm/src/main/java/com/spring/orm/dao/StudentDao.java
@@ -0,0 +1,16 @@
+package com.spring.orm.dao;
+
+import org.springframework.orm.hibernate5.HibernateTemplate;
+
+import com.spring.orm.entites.Student;
+
+public class StudentDao {
+	
+	private HibernateTemplate hibernateTemplate;
+	
+	public int insert(Student student) {
+		Integer i = (Integer) this.hibernateTemplate.save(student);
+		return i;
+	}
+
+}
